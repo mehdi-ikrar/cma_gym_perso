8a2ef502e3cf8bf3c9f805bd6e565f9607416409
mehdi-ikrar <ikrar.mehdi@gmail.com>
2025-03-14 06:30:08 +0100

    mise à jour des chemins d'image des employés et modification du modèle pour stocker les images en tant que chaînes

--- src/back/data/employee.js
+++ src/back/data/employee.js
@@ -4,7 +4,7 @@ export const employee = [
       name: "Lascow",
       firstname: "Patricia",
       function: "Ces un directeur",
-      image: "employee.png",
+      image: "images/photo1.jpg",
       description: "Ces un coach."
     },
     {
@@ -12,7 +12,7 @@ export const employee = [
       name: "Speedrun",
       firstname: "John",
       function: "Coach",
-      image: "employee.png",
+      image: "images/image2.jpg",
       description: "Ces un coach."
     },
     {
@@ -20,7 +20,7 @@ export const employee = [
       name: "Completion",
       firstname: "Jane",
       function: "Coach",
-      image: "employee.png",
+      image: "images/image3.jpg",
       description: "Ces un coach."
     },
     {
@@ -28,7 +28,7 @@ export const employee = [
       name: "Damage",
       firstname: "Doe",
       function: "Coach",
-      image: "employee.png",
+      image: "images/image4.jpg",
       description: "Ces un coach."
     }
   ];
--- src/back/migrations/seedTables.js
+++ src/back/migrations/seedTables.js
@@ -1,6 +1,4 @@
 import "dotenv/config";
-import fs from 'fs';
-import path from 'path';  // Importer path
 import { sequelize } from "../models/db.client.js";
 import { Activity, Actuality, Documents, Employee, Admin } from "../models/associations.js";
 import { employee } from "../data/employee.js";
@@ -9,28 +7,23 @@ import { activity } from "../data/activity.js";
 import { actuality } from "../data/acutality.js";
 import { admin } from "../data/admin.js";
 
-// Calculer le répertoire de travail avec import.meta.url
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
 
 // Seed Employee table
-for (const emp of employee) {
-    const imagePath = path.join( 'data', 'images', emp.image);
-    const imageBuffer = fs.readFileSync(imagePath);  // Lire l'image en tant que Buffer
-
+for(const emp of employee){
     await Employee.create({
         id: emp.id,
         firstname: emp.firstname,
         name: emp.name,
         function: emp.function,
-        image: imageBuffer,  // Insérer l'image sous forme de Buffer dans la base de données
+        image: emp.image,
         description: emp.description,
+        
     });
 }
 console.log('Employees seeded');
 
 // Seed Activity table
-for (const act of activity) {
+for(const act of activity){
     await Activity.create({
         id: act.id,
         category: act.category,
@@ -42,7 +35,7 @@ for (const act of activity) {
 console.log('Activities seeded');
 
 // Seed Actuality table
-for (const act of actuality) {
+for(const act of actuality){
     await Actuality.create({
         id: act.id,
         title: act.title,
@@ -53,7 +46,7 @@ for (const act of actuality) {
 console.log('Actualities seeded');
 
 // Seed Documents table
-for (const doc of document) {
+for(const doc of document){
     await Documents.create({
         id: doc.id,
         title: doc.title,
@@ -63,7 +56,7 @@ for (const doc of document) {
 console.log('Documents seeded');
 
 // Seed User table with hashed passwords
-for (const u of admin) {
+for(const u of admin){
     await Admin.create({
         id: u.id,
         name: u.name,
@@ -74,4 +67,5 @@ for (const u of admin) {
 console.log('Admin seeded');
 
 // Create relationships between Employee and Activity (sample)
+
 await sequelize.close();
