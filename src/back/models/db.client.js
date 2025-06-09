cde79b23e1b960bc9b5a39e88984e27f7357f8ee
mehdi-ikrar <ikrar.mehdi@gmail.com>
2025-06-09 04:01:01 +0200

    Ajout des options de dialecte pour la connexion à PostgreSQL et désactivation des logs Sequelize

--- src/back/models/db.client.js
+++ src/back/models/db.client.js
@@ -4,6 +4,13 @@ import { Sequelize } from "sequelize";
 export const sequelize = new Sequelize(process.env.PG_URL, {
   dialect: "postgres",
   define: {
-    timestamps: true
-  }
+    timestamps: true,
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,  // Ignore le certificat auto-signé
+    },
+  },
+  logging: false, // optionnel : ça calme un peu les logs Sequelize
 });
